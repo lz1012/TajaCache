a68729ce4ecca5c937534d895c6b2653015da7f8
lz <982496307@qq.com>
2022-12-04 18:01:04 +0800

    test

--- index.go
+++ index.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -9,6 +10,13 @@ func main() {
 	fmt.Println("初始化项目")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("init"))
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		encoder := json.NewEncoder(w)
+		encoder.Encode(map[string]interface{}{
+			"code":    http.StatusOK,
+			"message": "登陆成功",
+		})
 	})
 	http.ListenAndServe(":80", nil)
 }
