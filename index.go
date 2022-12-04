2b6f699a98ac1bea249cd33df25044cea7853f52
lz <982496307@qq.com>
2022-12-04 17:53:21 +0800

    第一个服务器

--- index.go
+++ index.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("初始化项目")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("init"))
+	})
+	http.ListenAndServe(":80", nil)
 }
